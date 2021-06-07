7bcd23e3d396c52db434aafe8b4dea1051b5db7e
PratikaNG <pratika.05ng@gmail.com>
2021-06-07 21:34:37 +0530

    app-working(beautifying left)

--- app.js
+++ app.js
@@ -2,11 +2,35 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// console.log(txtInput);
-function clickHandler(){
-    outputDiv.innerText="xxxx " + txtInput.value;
-    // console.log("input",txtInput.value);
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+	
+function getTranslationURL(text){
+    return serverURL  +"?" +"text=" + text
+    // var txtInputText = txtInput.value;  
 };
 
+
+//error handling
+function errorHandler(error){
+    console.log("error occured",error);
+    //alert("Something went wrong with the server.Please try again later");
+}
+
+
+function clickHandler(){
+    var inputText = txtInput.value; //taking input  
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => {
+        response.json();
+        console.log(response)
+    })
+    .then(data => {
+        var translatedText = data.contents.translated;
+        output.innerText = translatedText;
+    }).catch(errorHandler);
+}
+    
 btnTranslate = addEventListener("click",clickHandler);
 
